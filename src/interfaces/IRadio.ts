f9efb590e3529b65561e2139d9bb6678e2c4370e
Vitor Nogueira <vleonel96@gmail.com>
2024-10-25 10:10:17 -0300

    feat: Add IRadio interface

---
+++ src/interfaces/IRadio.ts
@@ -0,0 +1,41 @@
+interface IRadio {
+  changeuuid: string;
+  stationuuid: string;
+  serveruuid: string;
+  name: string;
+  url: string;
+  url_resolved: string;
+  homepage: string;
+  favicon: string;
+  tags: string;
+  country: string;
+  countrycode: string;
+  iso_3166_2: string | null;
+  state: string;
+  language: string;
+  languagecodes: string;
+  votes: number;
+  lastchangetime: string;
+  lastchangetime_iso8601: string;
+  codec: string;
+  bitrate: number;
+  hls: number;
+  lastcheckok: number;
+  lastchecktime: string;
+  lastchecktime_iso8601: string;
+  lastcheckoktime: string;
+  lastcheckoktime_iso8601: string;
+  lastlocalchecktime: string;
+  lastlocalchecktime_iso8601: string;
+  clicktimestamp: string;
+  clicktimestamp_iso8601: string;
+  clickcount: number;
+  clicktrend: number;
+  ssl_error: number;
+  geo_lat: number | null;
+  geo_long: number | null;
+  has_extended_info: boolean;
+}
+
+
+export default IRadio;
