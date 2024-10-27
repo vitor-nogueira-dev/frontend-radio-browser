48e19ad7dadd15b25a4ed984563a6a3ed0162868
Vitor Nogueira <vleonel96@gmail.com>
2024-10-27 06:41:01 -0300

    refactor: Update RadioListCard component to include data-cy attribute, improve accessibility, and update UI

--- src/components/RadioStationList/RadioListCard.tsx
+++ src/components/RadioStationList/RadioListCard.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { Card, CardContent } from '@/components/ui/card';
 import TooltipElement from '@/components/common/TooltipElement';
 import FavoriteButton from '@/components/common/FavoriteButton';
@@ -22,18 +24,17 @@ function RadioListCard({ radio, isFavorite, toggleFavorite }: RadioCardProps) {
       delayDuration={200}
       side={isMobile ? 'top' : 'right'}
     >
-      <Card className="mb-2 w-full min-w-[260px]">
+      <Card className="mb-2 w-full min-w-[260px] cursor-pointer hover:shadow-lg transition-all" data-cy={radio.stationuuid}>
         <CardContent className="flex items-center justify-between p-4">
-          <div className="flex items-center flex-1 max-w-32">
-            <RadioInfo radio={radio} />
-          </div>
+          <RadioInfo radio={radio} />
           <FavoriteButton
+            ariaLabel={isFavorite ? 'Remover favorito' : 'Adicionar favorito'}
             isFavorite={isFavorite}
             onClick={() => toggleFavorite(radio)}
           />
         </CardContent>
       </Card>
-    </TooltipElement>
+    </TooltipElement >
   );
 }
 
