3b68649eed9c71ff804e5bd6c98600892e6af450
Vitor Nogueira <vleonel96@gmail.com>
2024-10-27 06:40:01 -0300

    refactor: Update SearchInput component to include data-cy attribute and tooltip element

--- src/components/common/SearchInput.tsx
+++ src/components/common/SearchInput.tsx
@@ -3,32 +3,38 @@ import { Search, X } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import TooltipElement from '@/components/common/TooltipElement';
 
 interface SearchInputProps {
   search: string;
   handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   clearSearch: () => void;
+  dataCy: string;
 }
 
-function SearchInput({ search, handleInputChange, clearSearch }: SearchInputProps) {
+function SearchInput({ search, handleInputChange, clearSearch, dataCy }: SearchInputProps) {
   return (
     <div className="relative">
       <Input
-        className="pl-10 pr-4"
+        className="pl-10 pr-4 h-10"
         placeholder="Pesquise por nome"
         value={search}
+        data-cy={dataCy}
         onChange={handleInputChange}
       />
       <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={18} />
       {search && (
-        <Button
-          variant="ghost"
-          size="icon"
-          className="absolute right-2 top-1/2 transform -translate-y-1/2"
-          onClick={clearSearch}
-        >
-          <X className="h-4 w-4" />
-        </Button>
+        <TooltipElement title="Limpar pesquisa" delayDuration={200} side="top">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="absolute right-2 top-1/2 transform -translate-y-1/2 rounded-full"
+            onClick={clearSearch}
+            data-cy={`${dataCy}-clear`}
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </TooltipElement>
       )}
     </div>
   );
